8a1649bf50ae27aeaf087776ac9f4363761e4915
Mikey Lintz <mlintz@gmail.com>
2014-12-20 19:11:29 -0800

    asdf

---
+++ Point/Experimental/Model/PTAFileOperationAggregator.h
@@ -0,0 +1,13 @@
+//
+//  PTAFileOperationAggregator.h
+//  Point
+//
+//  Created by Mikey Lintz on 12/18/14.
+//  Copyright (c) 2014 Mikey Lintz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PTAFileOperationAggregator : NSObject
+
+@end
---
+++ Point/Experimental/Model/PTAFileOperationAggregator.m
@@ -0,0 +1,13 @@
+//
+//  PTAFileOperationAggregator.m
+//  Point
+//
+//  Created by Mikey Lintz on 12/18/14.
+//  Copyright (c) 2014 Mikey Lintz. All rights reserved.
+//
+
+#import "PTAFileOperationAggregator.h"
+
+@implementation PTAFileOperationAggregator
+
+@end
